df57608bc1cc4b3681390929caf0effda84ad784
Sheldon Juncker <sheldonjuncker@gmail.com>
2022-01-28 18:20:32 -0500

    renaming file.

--- src/main/java/com/dreamcloud/esa_score/analysis/TfIdfStrategyFactory.java
+++ src/main/java/com/dreamcloud/esa_score/analysis/TfIdfStrategyFactory.java
@@ -2,7 +2,7 @@ package com.dreamcloud.esa_score.analysis;
 
 import com.dreamcloud.esa_score.analysis.strategy.TfIdfStrategy;
 
-public class TfidfStrategyFactory {
+public class TfIdfStrategyFactory {
     public TfIdfStrategy getStrategy(TfIdfOptions options) {
         TfIdfStrategy calculator = new TfIdfCalculator(options.getMode());
         if (options.isBm25()) {
---
+++ src/main/java/com/dreamcloud/esa_score/cli/FileSystemScoringReader.java
@@ -0,0 +1,101 @@
+package com.dreamcloud.esa_score.cli;
+
+import com.dreamcloud.esa_score.analysis.CollectionInfo;
+import com.dreamcloud.esa_score.fs.*;
+import com.dreamcloud.esa_score.score.DocumentScoreCachingReader;
+import com.dreamcloud.esa_score.score.DocumentScoreReader;
+import com.dreamcloud.esa_score.score.LoggingScoreReader;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileSystemScoringReader {
+    private static String TERM_INDEX_FILE = "term-index";
+    private static String DOCUMENT_SCORE_FILE = "document-scores";
+    private static String SCORE_MECHANISM = "score-mechanism";
+    private static String SCORE_CACHE = "score-cache";
+
+    private File termIndexFile;
+    private File documentScoreFile;
+    private String scoreMechanism;
+    private String scoreCache;
+
+
+    private static TermIndex termIndex;
+
+    public void addOptions(Options options) {
+        Option option = new Option(null, TERM_INDEX_FILE, true, "");
+        option.setRequired(false);
+        options.addOption(option);
+
+        option = new Option(null, DOCUMENT_SCORE_FILE, true, "");
+        option.setRequired(false);
+        options.addOption(option);
+
+        option = new Option(null, SCORE_MECHANISM, true, "");
+        option.setRequired(false);
+        options.addOption(option);
+
+        option = new Option(null, SCORE_CACHE, true, "");
+        option.setRequired(false);
+        options.addOption(option);
+    }
+
+    public void parseOptions(CommandLine cli) {
+        this.termIndexFile = new File(cli.getOptionValue(TERM_INDEX_FILE, "term-index.dc"));
+        this.documentScoreFile = new File(cli.getOptionValue(DOCUMENT_SCORE_FILE, "document-scores.dc"));
+        this.scoreMechanism = cli.getOptionValue(SCORE_MECHANISM, "disk");
+        this.scoreCache = cli.getOptionValue(SCORE_CACHE, "");
+    }
+
+    private TermIndex getTermIndex() throws IOException {
+        if (termIndex == null) {
+            TermIndexReader reader = new TermIndexReader();
+            reader.open(termIndexFile);
+            termIndex = reader.readIndex();
+        }
+        return termIndex;
+    }
+
+    public CollectionInfo getCollectionInfo() throws IOException {
+        TermIndex termIndex = getTermIndex();
+        return new CollectionInfo(termIndex.getDocumentCount(), termIndex.getAverageDocumentLength(), termIndex.getDocumentFrequencies());
+    }
+
+    public CollectionWriter getCollectionWriter() {
+        return new DiskCollectionWriter(termIndexFile, documentScoreFile);
+    }
+
+    public DocumentScoreReader getScoreReader() throws IOException {
+        DocumentScoreDataReader scoreDataReader;
+        switch(scoreMechanism) {
+            case "disk":
+                scoreDataReader = new DocumentScoreFileReader(documentScoreFile);
+                break;
+            case "memory":
+                scoreDataReader = new DocumentScoreMemoryReader(documentScoreFile);
+                break;
+            default:
+                throw new IllegalArgumentException("Score mechanism '" + scoreMechanism + "' is not supported.'");
+        }
+
+        TermIndex termIndex = getTermIndex();
+        DocumentScoreReader scoreReader = new ScoreReader(termIndex, scoreDataReader);
+
+
+        if (!"".equals(scoreCache)) {
+            switch(scoreCache) {
+                case "top-hits":
+                    scoreReader = new DocumentScoreCachingReader(scoreReader);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Score cache mechanism '" + scoreCache + "' is not supported.'");
+            }
+        }
+        return scoreReader;
+    }
+}
